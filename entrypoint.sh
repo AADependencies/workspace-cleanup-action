b5704c1063daac2720fc6ad15f67dbb0100d4311
Loki <lokesh.mydilse@gmail.com>
2022-05-19 10:50:47 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,16 +16,15 @@ WORKSPACE="/github/workspace"
 if [[ $INPUT_DIRECTORY == "NONE" ]]; then
   deleteMe "$WORKSPACE/.*"
   deleteMe "$WORKSPACE/*"
+  deleteMe "$RUNNER_TEMP/*"
+  deleteMe "$RUNNER_TEMP/.*"
 else
   if [[ -d ${WORKSPACE}/${INPUT_DIRECTORY} ]]; then
     deleteMe "${WORKSPACE}/${INPUT_DIRECTORY}"
   else
     echo "${INPUT_DIRECTORY} is not found inside the workspace hence deleting whole workspace"
-    printenv
     deleteMe "$WORKSPACE/.*"
     deleteMe "$WORKSPACE/*"
-    deleteMe "$RUNNER_TEMP/*"
-    deleteMe "$RUNNER_TEMP/.*"
   fi
 fi
 
